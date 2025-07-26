04ad93caeee272d7eadacd798283f710144b5270
Scott Bearden <18202628+scottbearden@users.noreply.github.com>
2025-07-26 18:57:49 -0400

    Remove SolidQueue process model and migration; update application configuration for Solid Queue

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module HotwireCal
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # Soild Cache and Queue
+    config.active_job.queue_adapter = :solid_queue
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_19_180947) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_19_175237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,18 +50,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_19_180947) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "solid_queue_processes", force: :cascade do |t|
-    t.string "name"
-    t.string "status"
-    t.datetime "last_run_at"
-    t.string "kind"
-    t.integer "pid"
-    t.string "hostname"
-    t.integer "supervisor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
