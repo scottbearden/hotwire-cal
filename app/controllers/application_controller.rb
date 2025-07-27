79e5b460d4db1522e3c5ebe76c62d40a40863d14
Scott Bearden <18202628+scottbearden@users.noreply.github.com>
2025-07-26 20:30:55 -0400

    Add Current model to manage user and timezone attributes; implement timezone reset logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_current_details
+
+  private
+
+  def set_current_details
+    Current.timezone = Current.user.timezone if Current.user.present?
+  end
 end
